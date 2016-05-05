9a1aa2c23b66c94c6ea1d173664eec0c8f6845ca
Jason Hewison <jason.hewison@flashtalking.com>
2016-05-05 22:08:54 +0100

    Updated to run eslint when saving a javascript file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,9 +2,13 @@ set EDITOR nvim
 set VISUAL nvim
 set fish_greeting ""
 
+set PATH node_modules/.bin $PATH
+
 # Local config.
 if [ -f ~/.local.fish ]
   . ~/.local.fish
 end
 
-alias vim "nvim"
+#alias vim "nvim"
+
+alias run_excel_importer "env SQS_CDN_PUBLISH_QUEUE=cm-cdn-publish-jasonh SQS_IMPORT_QUEUE=cm-excel-import-jasonh S3_BUCKET=cmexcel-filestorage-jasonh SNS_IMPORT_TOPIC=arn:aws:sns:eu-west-1:657464860012:cm-excel-import-jasonh CACHE_HOST=redispreview.local CACHE_PORT=6378 CAPI_URL=http://capi.local:81 FILESIZE_URL=http://filesize.local:88 python3 importer.py"
