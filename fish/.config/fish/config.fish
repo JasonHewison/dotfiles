5f44f5c5b1b34df2d11d54b07af64fb80279b80b
Jason Hewison <jason.hewison@flashtalking.com>
2016-10-27 21:24:24 +0100

    Removing work stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,8 +14,3 @@ if [ -f ~/.local.fish ]
 end
 
 alias vim "nvim"
-
-alias run_excel_importer "env SQS_CDN_PUBLISH_QUEUE=cm-cdn-publish-jasonh SQS_IMPORT_QUEUE=cm-excel-import-jasonh S3_BUCKET=cmexcel-filestorage-jasonh SNS_IMPORT_TOPIC=arn:aws:sns:eu-west-1:657464860012:cm-excel-import-jasonh CACHE_HOST=redispreview.local CACHE_PORT=6378 CAPI_URL=http://capi.local:81 FILESIZE_URL=http://filesize.local:88 python3 importer.py"
-
-alias git-work "git config --global user.email jason.hewison@flashtalking.com"
-alias git-home "git config --global user.email jhewison@gmail.com"
